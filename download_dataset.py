8baa04d2c17827eba7a231a70337cf5d2c1cb189
vibhorjoshi <105739194+vibhorjoshi@users.noreply.github.com>
2024-06-01 18:12:34 +0700

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,16 +1,8 @@
-from datasets import load_dataset, DatasetDict
+from datasets import load_dataset
+import pandas as pd
 
-# Load the dataset from Hugging Face
-dataset = load_dataset("torchgeo/tropical_cyclone")
-
-# Perform a train-test split
-train_test_split = dataset['train'].train_test_split(test_size=0.2)
-
-# Create a DatasetDict with train and test sets
-dataset = DatasetDict({
-    'train': train_test_split['train'],
-    'test': train_test_split['test']
-})
-
-# Check the splits
-print(dataset)
+def load_cyclone_data():
+    dataset = load_dataset('torchgeo/tropical_cyclone', ignore_verifications=True)
+    df_train = pd.DataFrame(dataset['train'])
+    df_test = pd.DataFrame(dataset['test'])
+    return df_train, df_test
